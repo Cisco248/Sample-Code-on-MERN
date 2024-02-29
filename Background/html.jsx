ba96d5b08e9b21a414051b0cb60a5bf92c3f60f5
Lahiru Dilshan <129523905+Cisco248@users.noreply.github.com>
2024-03-01 04:20:44 +0530

    bg

---
+++ Background/html.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function html() {
+  return (
+    <div>
+      <div class="container">
+        <div class="blob-c">
+          <div class="shape-blob"></div>
+          <div class="shape-blob one"></div>
+          <div class="shape-blob two"></div>
+          <div class="shape-blob three"></div>
+          <div class="shape-blob four"></div>
+          <div class="shape-blob five"></div>
+          <div class="shape-blob six"></div>
+          <h1>Undulating Background Blobs</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default html;
